f47847e215b1b45336869ef26c9801dbca6a92a6
robbysmamita <robbysmamita@gmail.com>
2020-11-25 10:10:31 +0700

    add function live chat

--- application/views/layouts/footer.php
+++ application/views/layouts/footer.php
@@ -1,14 +1,13 @@
+<hr>
+<footer>
+	<p class="text-center">&#169 Copyright Kafeins | Allright Reserved | Version 2020</p>
+</footer>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+	integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+</script>
+<script src="<?=base_url() ?>assets/js/bootstrap.js"></script>
 
-    <hr>
-    <footer>
-        <p class="text-center">&#169 Copyright Kafeins | Allright Reserved | Version 2020</p>
-    </footer>
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
-    </script>
-    <script src="<?=base_url() ?>assets/js/bootstrap.js"></script>
-
-    <!-- Option 2: jQuery, Popper.js, and Bootstrap JS <script
+<!-- Option 2: jQuery, Popper.js, and Bootstrap JS <script
         src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
         integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
         crossorigin="anonymous"></script> <script
@@ -18,6 +17,25 @@
         src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js"
         integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s"
         crossorigin="anonymous"></script> -->
+
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+	var Tawk_API = Tawk_API || {},
+		Tawk_LoadStart = new Date();
+	(function () {
+		var s1 = document.createElement("script"),
+			s0 = document.getElementsByTagName("script")[0];
+		s1.async = true;
+		s1.src = 'https://embed.tawk.to/5fbdc65e920fc91564ca5bff/1enulmu6r';
+		s1.charset = 'UTF-8';
+		s1.setAttribute('crossorigin', '*');
+		s0.parentNode.insertBefore(s1, s0);
+	})();
+
+</script>
+<!--End of Tawk.to Script-->
+
+
 </body>
 
 </html>
